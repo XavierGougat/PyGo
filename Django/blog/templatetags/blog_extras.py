7df4ab633fdb42845c41716024b40fcf76d9245f
XavierGougat <xaviergougat@gmail.com>
2019-08-05 19:48:31 +0200

    remove blog

--- Django/blog/templatetags/blog_extras.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from django.utils.html import escape
-
-register = template.Library()
-
-
-@register.filter(is_safe=True)
-def citation(texte):
-    return "&laquo; {} &raquo;".format(escape(texte))
--- Django/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.conf.urls import url
-from blog import views
-from django.views.generic import ListView
-
-urlpatterns = [
-    url(r'^categorie/(?P<id>\d+)', views.ListeArticles.as_view(), name="blog_categorie"),
-    url(r'^article/(?P<pk>\d+)$', views.LireArticle.as_view(), name='blog_lire'),
-]
